5dabd9dcc7d81170cfec422a9c7ceb520b9d5b10
s.sriramprasath@gmail.com <s.sriramprasath@gmail.com>
2020-11-26 14:21:46 +0530

    search_control

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,9 +1,9 @@
-import {AfterViewInit, Component, ViewChild,OnInit} from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { FormGroup, FormControl,FormBuilder,Validators } from '@angular/forms';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 var ELEMENT_DATA: any = [];
 @Component({
@@ -13,21 +13,21 @@ var ELEMENT_DATA: any = [];
 })
 export class ProductComponent implements OnInit {
 
-  displayedColumns:any = ['productname','date'];  
-  dataSource = new MatTableDataSource(ELEMENT_DATA);  
-  @ViewChild(MatPaginator) paginator: MatPaginator;  
-  
+  displayedColumns: any = ['productname', 'date'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   productForm: FormGroup;
-  constructor(public api:ApiService,public formBuilder: FormBuilder) { }
+  constructor(public api: ApiService, public formBuilder: FormBuilder) { }
   // profileForm:FormControl
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
       productname: ['', Validators.required],
-      // rate: ['', Validators.required],
-      // qty: ['', Validators.required],     
+      grams: ['', Validators.required],
+      ProdandGram: ['', Validators.required],
       date: ['', Validators.required]
-      
-  });
+
+    });
 
     this.getproduct();
   }
@@ -37,48 +37,48 @@ export class ProductComponent implements OnInit {
   }
 
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;       
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-      console.log(this.dataSource.filter) 
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource.filter)
   }
-  
+
   getTotalCost() {
     // return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
   }
-  getproduct(){
-    this.dataSource = new MatTableDataSource();  
-    this.api.Getproduct().then((data:any) =>{
-    console.log(data.data.length)
-    if(data.data.length == 0){
-       this.api.snackmsg("No Record(s) Found","Close")
-    }
-    if( data.status == true && data.data.length != 0){
-      console.log(data)      
-      this.dataSource = new MatTableDataSource(data.data);
-   }
-console.log(data);
+  getproduct() {
+    this.dataSource = new MatTableDataSource();
+    this.api.Getproduct().then((data: any) => {
+      console.log(data.data.length)
+      if (data.data.length == 0) {
+        this.api.snackmsg("No Record(s) Found", "Close")
+      }
+      if (data.status == true && data.data.length != 0) {
+        console.log(data)
+        this.dataSource = new MatTableDataSource(data.data);
+      }
+      console.log(data);
 
-  }).catch(err =>{
-    // this.api.snackmsg("Hail","close")
-  })
-}
-
-onSubmit() {
-  
-  this.productForm.controls.date.setValue((new Date()).toLocaleDateString('en-GB'));
-
-  if(this.productForm.value.productname == ""){
-    this.api.snackmsg("Enter The Product Name","close")
-    return;
+    }).catch(err => {
+      // this.api.snackmsg("Hail","close")
+    })
   }
-  this.api.Postproduct(this.productForm.value).then((data)=>{
-    console.log(data)
-    this.getproduct();
-    this.productForm.reset();
-  }).catch(e =>{
-    console.log(e)
-    this.getproduct();
-  })
-  
-}
+
+  onSubmit() {
+    this.productForm.controls.date.setValue((new Date()).toLocaleDateString('en-GB'));
+    this.productForm.controls.ProdandGram.setValue(this.productForm.value.productname + "" + this.productForm.value.grams);
+    console.log(this.productForm.value)
+    if (this.productForm.value.productname == "") {
+      this.api.snackmsg("Enter The Product Name", "close")
+      return;
+    }
+    this.api.Postproduct(this.productForm.value).then((data)=>{
+      console.log(data)
+      this.getproduct();
+      this.productForm.reset();
+    }).catch(e =>{
+      console.log(e)
+      this.getproduct();
+    })
+
+  }
 }
